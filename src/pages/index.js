4612cefd669d97dfd6b2eeed51273718ad2aa61f
Jack Maden <jamaden92@gmail.com>
2019-09-15 21:54:13 +0100

    final

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,6 +8,8 @@ import SEO from '../components/SEO'
 import Layout from '../components/layout'
 import { SubscriptionForm } from '../components/subscriptionForm'
 import { WindowHeightWrapper } from '../components/windowHeightWrapper'
+import PanelReadingList from '../components/panelReadingList'
+import PanelAboutUs from '../components/panelAboutUs'
 
 export default ({ data }) => {
     const { edges } = data.allMarkdownRemark
@@ -34,34 +36,8 @@ export default ({ data }) => {
                 </div>
                 <animated.p className="time break page-center" style={fadeAfter}><AnchorLink href="#first-panel" style={{ textDecoration: "none" }}><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 500 500"><defs><linearGradient id="eed70a5d-3465-468c-b304-842a4dc6abf4" x1="90.9" y1="409.1" x2="409.1" y2="90.9" gradientUnits="userSpaceOnUse"><stop offset="0.35" stopColor="#1aa9ff"/><stop offset="0.44" stopColor="#22acff"/><stop offset="0.58" stopColor="#37b4ff"/><stop offset="0.75" stopColor="#5ac1ff"/><stop offset="0.89" stopColor="#7cceff"/></linearGradient></defs><title>Philosophy Break</title><g id="a694bf81-a4e3-4986-9301-cf65ad1fcac0" data-name="Layer 1"><path d="M250,499.5A249.5,249.5,0,0,1,73.58,73.58,249.5,249.5,0,1,1,426.42,426.42,247.84,247.84,0,0,1,250,499.5Z" fill="#fff"/><path d="M250,1A248.93,248.93,0,0,1,426.07,426.07a249,249,0,1,1-273-405.5A247.32,247.32,0,0,1,250,1m0-1C111.93,0,0,111.93,0,250S111.93,500,250,500,500,388.07,500,250,388.07,0,250,0Z" fill="#fff"/><circle cx="250" cy="250" r="225" fill="url(#eed70a5d-3465-468c-b304-842a4dc6abf4)"/></g><g id="4dca974f-b6d8-4b4f-b13a-2551c52acb2b" data-name="Layer 3"><path d="M462,325l-1.24,3.94C428.79,414.27,346.49,475,250,475c-98,0-181.41-62.69-212.25-150.16L461,325"/></g><g id="1a30999d-582a-4788-8e9a-e662ea057375" data-name="Layer 2"><polygon points="38 325 103 225 152 281 250 112 346 221 377 182 469 306 38 325" fill="#fff"/></g><g id="93c33bb2-e13c-4346-ae21-ff33957a2372" data-name="Layer 4"><polyline points="38 326 108 250.66 152 278.91 250 148 346 221.46 377 208.28 469 306.22 462 326"/><path d="M103.5,134.51c.26.33,4.76,5.92,12,5.14a14,14,0,0,0,10.29-6.86" fill="none" stroke="#000" strokeMiterlimit="10" strokeWidth="6"/><path d="M134.83,153.38c.26.33,4.76,5.92,12,5.14,6.75-.72,10-6.33,10.29-6.86" fill="none" stroke="#000" strokeMiterlimit="10" strokeWidth="6"/><path d="M159.7,124.22c.26.33,4.76,5.91,12,5.14A14,14,0,0,0,182,122.5" fill="none" stroke="#000" strokeMiterlimit="10" strokeWidth="6"/></g></svg><span style={{color: '#fff'}} className="bottom-blue-border">SCROLL TO EXPLORE&nbsp;&nbsp;<svg className="bounce" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="#fff" d="M207.029 381.476L12.686 187.132c-9.373-9.373-9.373-24.569 0-33.941l22.667-22.667c9.357-9.357 24.522-9.375 33.901-.04L224 284.505l154.745-154.021c9.379-9.335 24.544-9.317 33.901.04l22.667 22.667c9.373 9.373 9.373 24.569 0 33.941L240.971 381.476c-9.373 9.372-24.569 9.372-33.942 0z"/></svg></span></AnchorLink></animated.p>
             </WindowHeightWrapper>
-            <div id="first-panel" className="panel home">
-                <div className="darkener"></div>
+            <div id="first-panel" className="after-article grey-background">
                 <div className="page-center">
-                    <h2>Dive Deeper</h2>
-                    <div className="separator"></div>
-                    <p>Ready to deepen your knowledge? We've compiled reading lists of the major philosophical works you need to read.</p>
-                    <Link className="button primary" to="/reading-lists"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path fill="#fff" d="M542.22 32.05c-54.8 3.11-163.72 14.43-230.96 55.59-4.64 2.84-7.27 7.89-7.27 13.17v363.87c0 11.55 12.63 18.85 23.28 13.49 69.18-34.82 169.23-44.32 218.7-46.92 16.89-.89 30.02-14.43 30.02-30.66V62.75c.01-17.71-15.35-31.74-33.77-30.7zM264.73 87.64C197.5 46.48 88.58 35.17 33.78 32.05 15.36 31.01 0 45.04 0 62.75V400.6c0 16.24 13.13 29.78 30.02 30.66 49.49 2.6 149.59 12.11 218.77 46.95 10.62 5.35 23.21-1.94 23.21-13.46V100.63c0-5.29-2.62-10.14-7.27-12.99z"/></svg>Explore Reading Lists</Link>
-                </div>
-            </div>
-            <div className="panel home bottom">
-                <Img className="title-img" fluid={data.about.childImageSharp.fluid} alt="about philosophy break" />
-                <div className="page-center">
-                    <h2>Learn About Us</h2>
-                    <div className="separator"></div>
-                    <p>We're on a mission to get more people engaged with philosophy. See why we think that's so important — and how you can get involved.</p>
-                    <Link className="button primary" to="/about"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="#fff" d="M256 8C119.043 8 8 119.083 8 256c0 136.997 111.043 248 248 248s248-111.003 248-248C504 119.083 392.957 8 256 8zm0 448c-110.532 0-200-89.431-200-200 0-110.495 89.472-200 200-200 110.491 0 200 89.471 200 200 0 110.53-89.431 200-200 200zm107.244-255.2c0 67.052-72.421 68.084-72.421 92.863V300c0 6.627-5.373 12-12 12h-45.647c-6.627 0-12-5.373-12-12v-8.659c0-35.745 27.1-50.034 47.579-61.516 17.561-9.845 28.324-16.541 28.324-29.579 0-17.246-21.999-28.693-39.784-28.693-23.189 0-33.894 10.977-48.942 29.969-4.057 5.12-11.46 6.071-16.666 2.124l-27.824-21.098c-5.107-3.872-6.251-11.066-2.644-16.363C184.846 131.491 214.94 112 261.794 112c49.071 0 101.45 38.304 101.45 88.8zM298 368c0 23.159-18.841 42-42 42s-42-18.841-42-42 18.841-42 42-42 42 18.841 42 42z"/></svg>Find Out More</Link>
-                </div>
-            </div>
-            {/*after article*/}
-            <div className="after-article grey-background">
-                <div className="page-center">
-                    <h3>Get Weekly Breaks</h3>
-                    <div className="separator"></div>
-                </div>
-                <SubscriptionForm />
-                <div className="page-center">
-                    <h3>Latest Breaks</h3>
-                    <div className="separator" style={{margin: "1.5em auto 2em"}}></div>
                     {edges.slice(0, 4).map(edge => {
                         //curly brackets around variable is a destructuring assignment - e.g. the below equals edge.node.frontmatter & also edge.node.timeToRead
                         const {frontmatter, timeToRead, fields} = edge.node;
@@ -77,7 +53,38 @@ export default ({ data }) => {
                         </div>
                         )
                     })}
+                    <p className="text-center"><Link className="button primary" to="/articles"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M504 256C504 119 393 8 256 8S8 119 8 256s111 248 248 248 248-111 248-248zm-448 0c0-110.5 89.5-200 200-200s200 89.5 200 200-89.5 200-200 200S56 366.5 56 256zm72 20v-40c0-6.6 5.4-12 12-12h116v-67c0-10.7 12.9-16 20.5-8.5l99 99c4.7 4.7 4.7 12.3 0 17l-99 99c-7.6 7.6-20.5 2.2-20.5-8.5v-67H140c-6.6 0-12-5.4-12-12z"/></svg>View All Breaks</Link></p>
                 </div>
+            </div>
+            <PanelReadingList />
+            <div className="grey-background">
+                <div className="page-center">
+                    <Link className="reading-list" to="/reading-lists/consciousness">
+                        <Img className="title-img" fluid={data.consciousnesslist.childImageSharp.fluid} alt="consciousness reading list" />
+                        <div className="darkener"></div>
+                        <div className="reading-list-title">
+                            <h3>Consciousness</h3>
+                            <p style={{margin: 0}}><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path fill="#fff" d="M542.22 32.05c-54.8 3.11-163.72 14.43-230.96 55.59-4.64 2.84-7.27 7.89-7.27 13.17v363.87c0 11.55 12.63 18.85 23.28 13.49 69.18-34.82 169.23-44.32 218.7-46.92 16.89-.89 30.02-14.43 30.02-30.66V62.75c.01-17.71-15.35-31.74-33.77-30.7zM264.73 87.64C197.5 46.48 88.58 35.17 33.78 32.05 15.36 31.01 0 45.04 0 62.75V400.6c0 16.24 13.13 29.78 30.02 30.66 49.49 2.6 149.59 12.11 218.77 46.95 10.62 5.35 23.21-1.94 23.21-13.46V100.63c0-5.29-2.62-10.14-7.27-12.99z"/></svg>&nbsp;Essential Reading List</p>
+                        </div>
+                    </Link>
+                    <Link className="reading-list" to="/reading-lists/history-of-western-philosophy">
+                        <Img className="title-img" fluid={data.whistorylist.childImageSharp.fluid} alt="history of western philosophy reading list" />
+                        <div className="darkener"></div>
+                        <div className="reading-list-title">
+                            <h3>History of Western Philosophy</h3>
+                            <p style={{margin: 0}}><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path fill="#fff" d="M542.22 32.05c-54.8 3.11-163.72 14.43-230.96 55.59-4.64 2.84-7.27 7.89-7.27 13.17v363.87c0 11.55 12.63 18.85 23.28 13.49 69.18-34.82 169.23-44.32 218.7-46.92 16.89-.89 30.02-14.43 30.02-30.66V62.75c.01-17.71-15.35-31.74-33.77-30.7zM264.73 87.64C197.5 46.48 88.58 35.17 33.78 32.05 15.36 31.01 0 45.04 0 62.75V400.6c0 16.24 13.13 29.78 30.02 30.66 49.49 2.6 149.59 12.11 218.77 46.95 10.62 5.35 23.21-1.94 23.21-13.46V100.63c0-5.29-2.62-10.14-7.27-12.99z"/></svg>&nbsp;Essential Reading List</p>
+                        </div>
+                    </Link>
+                    <p className="text-center"><Link className="button primary" to="/reading-lists"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path fill="#fff" d="M542.22 32.05c-54.8 3.11-163.72 14.43-230.96 55.59-4.64 2.84-7.27 7.89-7.27 13.17v363.87c0 11.55 12.63 18.85 23.28 13.49 69.18-34.82 169.23-44.32 218.7-46.92 16.89-.89 30.02-14.43 30.02-30.66V62.75c.01-17.71-15.35-31.74-33.77-30.7zM264.73 87.64C197.5 46.48 88.58 35.17 33.78 32.05 15.36 31.01 0 45.04 0 62.75V400.6c0 16.24 13.13 29.78 30.02 30.66 49.49 2.6 149.59 12.11 218.77 46.95 10.62 5.35 23.21-1.94 23.21-13.46V100.63c0-5.29-2.62-10.14-7.27-12.99z"/></svg>View All Reading Lists</Link></p>
+                </div>
+            </div>
+            <PanelAboutUs />
+            <div className="after-article grey-background">
+                <div className="page-center">
+                    <h3>Get Weekly Breaks</h3>
+                    <div className="separator"></div>
+                </div>
+                <SubscriptionForm />
             </div>
         </Layout>
         </>
@@ -93,9 +100,16 @@ export const query = graphql`
                 }
             }
         }
-        about: file(relativePath: {eq: "aboutphilflip.jpg"}) {
+        consciousnesslist: file(relativePath: {eq: "consciousness-list.jpg"}) {
             childImageSharp {
-                fluid(maxWidth: 2100) {
+                fluid(maxWidth: 600) {
+                    ...GatsbyImageSharpFluid
+                }
+            }
+        }
+        whistorylist: file(relativePath: {eq: "whistorylist.jpg"}) {
+            childImageSharp {
+                fluid(maxWidth: 600) {
                     ...GatsbyImageSharpFluid
                 }
             }
