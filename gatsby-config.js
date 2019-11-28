b4f092b4cc6f1cc315d3a0302e041de1bf924736
Jack Maden <jamaden92@gmail.com>
2019-11-28 15:21:17 +0000

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -23,7 +23,7 @@ module.exports = {
         {
             resolve: 'gatsby-plugin-robots-txt',
             options: {
-              policy: [{ userAgent: '*', allow: '/' }]
+              policy: [{ userAgent: '*', Disallow: '/thank' }]
             }
         },
         //html lang
