2cea1719459f90abdd61bc4ebc3385003625a792
Jack Maden <jamaden92@gmail.com>
2021-02-25 13:25:22 +0000

    lbq3

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import React from "react"
 import { graphql, Link } from "gatsby"
 import Img from "gatsby-image"
+import AnchorLink from 'react-anchor-link-smooth-scroll'
 
 import SEO from '../components/SEO'
 import Layout from '../components/layout'
@@ -19,13 +20,13 @@ export default ({ data }) => {
     let greet = 'Hello there, Philosopher' // variable for message & set default
 
     if (hrs < 12) { //if morning
-    greet = 'Good Morning, Philosopher'
+        greet = 'Good Morning, Philosopher'
     }
     else if (hrs >= 12 && hrs < 17) { //if afternoon
-    greet = 'Good Afternoon, Philosopher'
+        greet = 'Good Afternoon, Philosopher'
     }
     else if (hrs >= 17 && hrs < 24) { //if evening
-    greet = 'Good Evening, Philosopher'
+        greet = 'Good Evening, Philosopher'
     }
 
     return (
@@ -34,27 +35,30 @@ export default ({ data }) => {
         <Layout>
         <Navigation bg="light-bg" />
             {/*page title*/}
-            <div className="top-title text-center">
+            <div className="top-title text-center ">
                 <div className="page-center">
+                    <div className="home-svg"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 500 500"><defs><linearGradient id="eed70a5d-3465-468c-b304-842a4dc6abf4" x1="90.9" y1="409.1" x2="409.1" y2="90.9" gradientUnits="userSpaceOnUse"><stop offset="0.35" stopColor="#1aa9ff"/><stop offset="0.44" stopColor="#22acff"/><stop offset="0.58" stopColor="#37b4ff"/><stop offset="0.75" stopColor="#5ac1ff"/><stop offset="0.89" stopColor="#7cceff"/></linearGradient></defs><title>Philosophy Break</title><g id="a694bf81-a4e3-4986-9301-cf65ad1fcac0" data-name="Layer 1"><path d="M250,499.5A249.5,249.5,0,0,1,73.58,73.58,249.5,249.5,0,1,1,426.42,426.42,247.84,247.84,0,0,1,250,499.5Z" fill="#fff"/><path d="M250,1A248.93,248.93,0,0,1,426.07,426.07a249,249,0,1,1-273-405.5A247.32,247.32,0,0,1,250,1m0-1C111.93,0,0,111.93,0,250S111.93,500,250,500,500,388.07,500,250,388.07,0,250,0Z" fill="#fff"/><circle cx="250" cy="250" r="225" fill="url(#eed70a5d-3465-468c-b304-842a4dc6abf4)"/></g><g id="4dca974f-b6d8-4b4f-b13a-2551c52acb2b" data-name="Layer 3"><path d="M462,325l-1.24,3.94C428.79,414.27,346.49,475,250,475c-98,0-181.41-62.69-212.25-150.16L461,325"/></g><g id="1a30999d-582a-4788-8e9a-e662ea057375" data-name="Layer 2"><polygon points="38 325 103 225 152 281 250 112 346 221 377 182 469 306 38 325" fill="#fff"/></g><g id="93c33bb2-e13c-4346-ae21-ff33957a2372" data-name="Layer 4"><polyline points="38 326 108 250.66 152 278.91 250 148 346 221.46 377 208.28 469 306.22 462 326"/><path d="M103.5,134.51c.26.33,4.76,5.92,12,5.14a14,14,0,0,0,10.29-6.86" fill="none" stroke="#000" strokeMiterlimit="10" strokeWidth="6"/><path d="M134.83,153.38c.26.33,4.76,5.92,12,5.14,6.75-.72,10-6.33,10.29-6.86" fill="none" stroke="#000" strokeMiterlimit="10" strokeWidth="6"/><path d="M159.7,124.22c.26.33,4.76,5.91,12,5.14A14,14,0,0,0,182,122.5" fill="none" stroke="#000" strokeMiterlimit="10" strokeWidth="6"/></g></svg></div>
                     <h1 id="home-page">{greet}</h1>
                     <div className="separator"></div>
                     <p className="mid-width large-grey-font">Welcome to Philosophy Break, a place to enrich your mind with philosophy's best questions, quotes, and ideas.</p>
+                    <p className="time break text-center small-mar"><svg className="bounce" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="#d2d4d9" d="M207.029 381.476L12.686 187.132c-9.373-9.373-9.373-24.569 0-33.941l22.667-22.667c9.357-9.357 24.522-9.375 33.901-.04L224 284.505l154.745-154.021c9.379-9.335 24.544-9.317 33.901.04l22.667 22.667c9.373 9.373 9.373 24.569 0 33.941L240.971 381.476c-9.373 9.372-24.569 9.372-33.942 0z"/></svg></p>
                 </div>
             </div>
-            <div className="large-mar">
-                <div className="two-col big-2-col row-reverse page-center">
-                    <div>
-                        <Img fluid={data.lbq.childImageSharp.fluid} fadeIn={false} loading="eager" alt="Life's Big Questions" />
-                    </div>
-                    <div className="mid-mar mobile-center">
-                        <span className="tag time" style={{color: "#2b2f33"}}><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512"><path d="M624 416H381.54c-.74 19.81-14.71 32-32.74 32H288c-18.69 0-33.02-17.47-32.77-32H16c-8.8 0-16 7.2-16 16v16c0 35.2 28.8 64 64 64h512c35.2 0 64-28.8 64-64v-16c0-8.8-7.2-16-16-16zM576 48c0-26.4-21.6-48-48-48H112C85.6 0 64 21.6 64 48v336h512V48zm-64 272H128V64h384v256z"/></svg>5-DAY COURSE</span><div className="separator course mobile-center-margin"></div>
+            <div className="mid-pad darkradial-background" id="first-panel">
+                <div className="two-col big-1-col row-reverse page-center">
+                    <div className="mobile-center">
+                        <span className="tag time"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512"><path fill="#fff" d="M624 416H381.54c-.74 19.81-14.71 32-32.74 32H288c-18.69 0-33.02-17.47-32.77-32H16c-8.8 0-16 7.2-16 16v16c0 35.2 28.8 64 64 64h512c35.2 0 64-28.8 64-64v-16c0-8.8-7.2-16-16-16zM576 48c0-26.4-21.6-48-48-48H112C85.6 0 64 21.6 64 48v336h512V48zm-64 272H128V64h384v256z"/></svg>5-DAY COURSE</span><div className="separator course mobile-center-margin"></div>
                         <h2>Start Answering Life's Big Questions</h2>
-                        <p className="large-grey-font">Why does anything exist? How should we approach life? Our celebrated introductory philosophy course distills philosophy's best answers to life's big questions, taking you on a whirlwind five-day journey of reflection, understanding, and discovery.</p>
+                        <Img className="desktop-no-display" fluid={data.lbq.childImageSharp.fluid} fadeIn={false} loading="eager" alt="Life's Big Questions" />
+                        <p className="large-grey-font">Why does anything exist? Do we have free will? How should we approach life? Our celebrated introductory philosophy course distills philosophy's best answers to life's big questions, taking you on a whirlwind five-day journey of reflection and discovery.</p>
                         <Link className="button primary" to="/lifes-big-questions/"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M504 256C504 119 393 8 256 8S8 119 8 256s111 248 248 248 248-111 248-248zm-448 0c0-110.5 89.5-200 200-200s200 89.5 200 200-89.5 200-200 200S56 366.5 56 256zm72 20v-40c0-6.6 5.4-12 12-12h116v-67c0-10.7 12.9-16 20.5-8.5l99 99c4.7 4.7 4.7 12.3 0 17l-99 99c-7.6 7.6-20.5 2.2-20.5-8.5v-67H140c-6.6 0-12-5.4-12-12z"/></svg>Explore Course Now</Link>
+                    </div>
+                    <div className="mobile-no-display">
+                        <Img fluid={data.lbq.childImageSharp.fluid} fadeIn={false} loading="eager" alt="Life's Big Questions" />
                     </div>
                 </div>   
             </div>         
-            <div id="first-panel" className="grey-background large-pad">
+            <div className="grey-background large-pad">
                 <HomeArticleList />
             </div>
             <SubscriptionForm />
