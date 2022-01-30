05893a7afeaeb62e776ff5413e6513d927375f4f
Jack Maden <jamaden92@gmail.com>
2022-01-30 16:24:24 +0000

    N-update

--- src/components/reviewsLBQ.js
+++ src/components/reviewsLBQ.js
@@ -13,33 +13,57 @@ export default function Reviews(props) {
                 <h2>{props.title}</h2>
                 <div className="two-col align-top">
                     <div className="small-mar">
+                        <p style={{marginBottom: "0.5em"}}><span style={{color: "#ffc536"}}>&#9733;&#9733;&#9733;&#9733;&#9733;</span>&nbsp;&nbsp;<span><i>Truly enjoyed</i></span></p>
+                        <span style={{lineHeight: "1.3"}}>Excellent course experience! Good pace and length of reads, combined with very well written content. Complexities are shared in a digestible manner. The reading lists are a great addition. I truly enjoyed every chapter. Thanks!</span>
+                        <div><p className="verified">VERIFIED BUYER</p>&nbsp;&nbsp;<span style={{color: "#737d88"}}>Tomas R. on 27 January 2022</span></div>
+                    </div>
+                    <div className="small-mar">
+                        <p style={{marginBottom: "0.5em"}}><span style={{color: "#ffc536"}}>&#9733;&#9733;&#9733;&#9733;&#9733;</span>&nbsp;&nbsp;<span><i>Very good</i></span></p>
+                        <span style={{lineHeight: "1.3"}}>Very good course experience. Well paced, and covered a very large area. Content was interesting and easy to read. It very nicely covered the promised topics. My favorite chapter was chapter five on ways of approaching life. It gave a great framework for further exploring what is important in life.</span>
+                        <div><p className="verified">VERIFIED BUYER</p>&nbsp;&nbsp;<span style={{color: "#737d88"}}>Chris J. on 15 January 2022</span></div>
+                    </div>
+                </div>
+                <div className="two-col align-top">
+                    <div className="small-mar">
                         <p style={{marginBottom: "0.5em"}}><span style={{color: "#ffc536"}}>&#9733;&#9733;&#9733;&#9733;&#9733;</span>&nbsp;&nbsp;<span><i>Excellent</i></span></p>
+                        <span style={{lineHeight: "1.3"}}>Excellent course experience. All the content was interesting, but my favorite chapter was chapter four on free will.</span>
+                        <div><p className="verified">VERIFIED BUYER</p>&nbsp;&nbsp;<span style={{color: "#737d88"}}>Amaro H. on 11 January 2022</span></div>
+                    </div>
+                    <div className="small-mar">
+                        <p style={{marginBottom: "0.5em"}}><span style={{color: "#ffc536"}}>&#9733;&#9733;&#9733;&#9733;&#9733;</span>&nbsp;&nbsp;<span><i>Brilliant</i></span></p>
+                        <span style={{lineHeight: "1.3"}}>Brilliant topics, well written and well delivered. Loved all the chapters. Overall a really good experience, thanks. </span>
+                        <div><p className="verified">VERIFIED BUYER</p>&nbsp;&nbsp;<span style={{color: "#737d88"}}>Philippa M. on 10 January 2022</span></div>
+                    </div>
+                </div>
+                <div className="two-col align-top">
+                    <div className="small-mar">
+                        <p style={{marginBottom: "0.5em"}}><span style={{color: "#ffc536"}}>&#9733;&#9733;&#9733;&#9733;&#9733;</span>&nbsp;&nbsp;<span><i>Enjoyable & contemplative</i></span></p>
                         <span style={{lineHeight: "1.3"}}>Excellent course. An enjoyable, contemplative read. The bite-sized portions allowed me to digest the information, and the content was very interesting and easy to understand — a good survey of philosophical ideas with references to books for digging deeper. I especially enjoyed the chapter on free will. As a financial planner, I’m trying to balance cognitive biases with behavioral change. This chapter provides good food for thought in how I approach my practice. Overall, great work.</span>
-                        <div><p className="verified">VERIFIED BUYER</p>&nbsp;&nbsp;<span style={{color: "#737d88"}}>David H. on 17 September 2021</span></div>
+                        <div><p className="verified">VERIFIED BUYER</p>&nbsp;&nbsp;<span style={{color: "#737d88"}}>David H. on 17 November 2021</span></div>
                     </div>
                     <div className="small-mar">
                         <p style={{marginBottom: "0.5em"}}><span style={{color: "#ffc536"}}>&#9733;&#9733;&#9733;&#9733;&#9733;</span>&nbsp;&nbsp;<span><i>Loved it</i></span></p>
                         <span style={{lineHeight: "1.3"}}>Thank you very much for giving me something to look forward to each day with the daily chapters — I loved it! Very refreshing to read something this clear and direct about how the great philosophers have approached some of the most difficult and enduring questions we can ask. Some truly interesting ideas I hadn't encountered before. Thanks!</span>
-                        <div><p className="verified">VERIFIED BUYER</p>&nbsp;&nbsp;<span style={{color: "#737d88"}}>Marie L. on 4 September 2021</span></div>
+                        <div><p className="verified">VERIFIED BUYER</p>&nbsp;&nbsp;<span style={{color: "#737d88"}}>Marie L. on 4 October 2021</span></div>
                     </div>
                 </div>
                 <div className="two-col align-top">
                     <div className="small-mar">
                         <p style={{marginBottom: "0.5em"}}><span style={{color: "#ffc536"}}>&#9733;&#9733;&#9733;&#9733;&#9733;</span>&nbsp;&nbsp;<span><i>Awesome</i></span></p>
                         <span style={{lineHeight: "1.3"}}>Great content. Receiving one chapter each day is awesome. As someone who is not a native English speaker, I found the content easy to grasp which is something I really appreciate. The first and second chapters were my favorite because they discuss the most fundamental questions humans can ask, but all of the chapters were extremely interesting.</span>
-                        <div><p className="verified">VERIFIED BUYER</p>&nbsp;&nbsp;<span style={{color: "#737d88"}}>Ahmed K. on 2 August 2021</span></div>
+                        <div><p className="verified">VERIFIED BUYER</p>&nbsp;&nbsp;<span style={{color: "#737d88"}}>Ahmed K. on 2 September 2021</span></div>
                     </div>
                     <div className="small-mar">
                         <p style={{marginBottom: "0.5em"}}><span style={{color: "#ffc536"}}>&#9733;&#9733;&#9733;&#9733;&#9733;</span>&nbsp;&nbsp;<span><i>Perfect difficulty level</i></span></p>
                         <span style={{lineHeight: "1.3"}}>Very good. Very well-written. Difficulty of the content is perfect for someone who might not be too familiar with philosophy so that is great too. Thank you!</span>
-                        <div><p className="verified">VERIFIED BUYER</p>&nbsp;&nbsp;<span style={{color: "#737d88"}}>Jonathon B. on 27 July 2021</span></div>
+                        <div><p className="verified">VERIFIED BUYER</p>&nbsp;&nbsp;<span style={{color: "#737d88"}}>Jonathon B. on 27 August 2021</span></div>
                     </div>
                 </div>
                 <div className="two-col align-top">
                     <div className="small-mar">
                         <p style={{marginBottom: "0.5em"}}><span style={{color: "#ffc536"}}>&#9733;&#9733;&#9733;&#9733;&#9733;</span>&nbsp;&nbsp;<span><i>Great job to everybody involved</i></span></p>
                         <span style={{lineHeight: "1.3"}}>It was great! Course material is written in a very accessible way that will appeal to any reader. The questions selected are the right ones, and they are presented in a nicely woven sequence. I can appreciate how hard it is to synthesize philosophy texts into such an easy to understand and nicely written text, so great job. One chapter per day represented a manageable time commitment that always left me excited for the next day, and my favorite chapter was the last one on how we should approach life. Reading what other thinkers had to say about it was very enriching and brought home the point that there is no universal answer. Great job to everybody involved in creating this course!</span>
-                        <div><p className="verified">VERIFIED BUYER</p>&nbsp;&nbsp;<span style={{color: "#737d88"}}>Julien S. on 23 June 2021</span></div>
+                        <div><p className="verified">VERIFIED BUYER</p>&nbsp;&nbsp;<span style={{color: "#737d88"}}>Julien S. on 23 July 2021</span></div>
                     </div>
                     <div className="small-mar">
                         <p style={{marginBottom: "0.5em"}}><span style={{color: "#ffc536"}}>&#9733;&#9733;&#9733;&#9733;&#9733;</span>&nbsp;&nbsp;<span><i>Very good introduction</i></span></p>
@@ -47,23 +71,24 @@ export default function Reviews(props) {
                         <div><p className="verified">VERIFIED BUYER</p>&nbsp;&nbsp;<span style={{color: "#737d88"}}>Sara B. on 15 June 2021</span></div>
                     </div>
                 </div>
-                <div className="two-col align-top">
-                    <div className="small-mar">
-                        <p style={{marginBottom: "0.5em"}}><span style={{color: "#ffc536"}}>&#9733;&#9733;&#9733;&#9733;&#9733;</span>&nbsp;&nbsp;<span><i>Really well put together</i></span></p>
-                        <span style={{lineHeight: "1.3"}}>Really well put together. A good resource to fill in gaps on a subject matter that I am interested in. Written in accessible language, the way more philosophy should be. Content was great & well laid out on my iPad. My favorite chapter was the 2nd chapter on if the world around us is ‘real’. It wove together Descartes, Berkeley, Locke, Hume, and Kant so well that it seemed the evolution was predestined.</span>
-                        <div><p className="verified">VERIFIED BUYER</p>&nbsp;&nbsp;<span style={{color: "#737d88"}}>Jared L. on 24 May 2021</span></div>
-                    </div>
-                    <div className="small-mar">
-                        <p style={{marginBottom: "0.5em"}}><span style={{color: "#ffc536"}}>&#9733;&#9733;&#9733;&#9733;&#9733;</span>&nbsp;&nbsp;<span><i>Excellent series</i></span></p>
-                        <span style={{lineHeight: "1.3"}}>Very positive course experience. Love the format - really nice to have something to look forward to each day with the daily chapters. The themes are discussed in a way that stimulates your interest without overwhelming you with information. Overall an excellent series, and I am sure I will keep returning to the chapters and hopefully doing some of the suggested further reading now my interest has been roused.</span>
-                        <div><p className="verified">VERIFIED BUYER</p>&nbsp;&nbsp;<span style={{color: "#737d88"}}>Alan M. on 21 May 2021</span></div>
-                    </div>
-                </div>
+                
                 
                 
                 
                 {/*load more reviews*/}
                 <div id="load-more" className={isActive ? null : "show"}>
+                    <div className="two-col align-top">
+                        <div className="small-mar">
+                            <p style={{marginBottom: "0.5em"}}><span style={{color: "#ffc536"}}>&#9733;&#9733;&#9733;&#9733;&#9733;</span>&nbsp;&nbsp;<span><i>Really well put together</i></span></p>
+                            <span style={{lineHeight: "1.3"}}>Really well put together. A good resource to fill in gaps on a subject matter that I am interested in. Written in accessible language, the way more philosophy should be. Content was great & well laid out on my iPad. My favorite chapter was the 2nd chapter on if the world around us is ‘real’. It wove together Descartes, Berkeley, Locke, Hume, and Kant so well that it seemed the evolution was predestined.</span>
+                            <div><p className="verified">VERIFIED BUYER</p>&nbsp;&nbsp;<span style={{color: "#737d88"}}>Jared L. on 24 May 2021</span></div>
+                        </div>
+                        <div className="small-mar">
+                            <p style={{marginBottom: "0.5em"}}><span style={{color: "#ffc536"}}>&#9733;&#9733;&#9733;&#9733;&#9733;</span>&nbsp;&nbsp;<span><i>Excellent series</i></span></p>
+                            <span style={{lineHeight: "1.3"}}>Very positive course experience. Love the format - really nice to have something to look forward to each day with the daily chapters. The themes are discussed in a way that stimulates your interest without overwhelming you with information. Overall an excellent series, and I am sure I will keep returning to the chapters and hopefully doing some of the suggested further reading now my interest has been roused.</span>
+                            <div><p className="verified">VERIFIED BUYER</p>&nbsp;&nbsp;<span style={{color: "#737d88"}}>Alan M. on 21 May 2021</span></div>
+                        </div>
+                    </div>
                     <div className="two-col align-top">
                         <div className="small-mar">
                             <p style={{marginBottom: "0.5em"}}><span style={{color: "#ffc536"}}>&#9733;&#9733;&#9733;&#9733;&#9733;</span>&nbsp;&nbsp;<span><i>Great primer</i></span></p>
--- src/pages/introduction-to-nietzsche/index.js
+++ src/pages/introduction-to-nietzsche/index.js
@@ -55,36 +55,85 @@ const Page = ({ data, location }) => {
                     </div>
                 </div>
             </div>
-            <div className="two-col page-center">
-                <div className="small-mar">
-                    <h3 className="flex-end"><span className="spectral big-letter">1</span><span>Life, Work, and Legacy</span></h3>
-                    <p className="small-grey-font">Who was Friedrich Nietzsche? What life did he lead that led him to have the ideas that he did? On day one we’ll set the scene and contextualize what’s to come by looking at Nietzsche the man, his influences, and outline all of his major works and chronology. </p>
-                </div>
-                <div className="small-mar">
-                    <h3 className="flex-end"><span className="spectral big-letter">2</span><span>The Apollonian & Dionysian</span></h3>
-                    <p className="small-grey-font">On day two we’ll start digging deeper into Nietzsche’s core ideas. We’ll begin with his early distinction between the Apollonian and Dionysian, which acts not only as a supremely illuminating way to consider life and art, but also as a precursor to his later ideas. </p>
-                </div>
-            </div>
-            <div className="two-col page-center">
-                <div className="small-mar">
-                    <h3 className="flex-end"><span className="spectral big-letter">3</span><span>Perspectivism</span></h3>
-                    <p className="small-grey-font">Do human beings have access to objective truth? Day three will consider Nietzsche’s fascinating theory of perspectivism, a highly sophisticated account of what truth is and how we can access it — an account that continues to excite scholars today. </p>
-                </div>
-                <div className="small-mar">
-                    <h3 className="flex-end"><span className="spectral big-letter">4</span><span>Master & Slave Morality</span></h3>
-                    <p className="small-grey-font">Where do morals come from? Nietzsche’s profound contribution to answering this question will be discussed on day four, where we’ll assess his takedown of traditional values like altruism, as well as his concepts of the ascetic ideal, master morality, and slave morality.</p>
+            <div className="small-mar">
+                <div className="two-col big-2-col page-center row-reverse">
+                    <div>
+                        <h3 className="flex-end"><span className="spectral big-letter">1</span><span>Life, Work, and Legacy</span></h3>
+                        <p className="spectral">Who was Friedrich Nietzsche? What life did he lead that led him to have the ideas that he did? On day one we’ll set the scene and contextualize what’s to come by looking at Nietzsche the man, his influences, and outline all of his major works and chronology.</p>
+                    </div>
+                    <div>
+                    <GatsbyImage
+                        image={data.chapter1.childImageSharp.gatsbyImageData}
+                        alt="Nietzsche’s Life, Work, and Legacy" />
+                    </div>
                 </div>
             </div>
-            <div className="two-col page-center">
-                <div className="small-mar">
-                    <h3 className="flex-end"><span className="spectral big-letter">5</span><span>God is Dead & the Übermensch</span></h3>
-                    <p className="small-grey-font">What should we value in a Godless world — and why? Nietzsche’s attempt to provide a ‘revaluation of values’ will be discussed on day five, where we’ll consider his famous ideas that God is Dead, the eternal recurrence, and the Übermensch (superman). </p>
-                </div>
-                <div className="small-mar">
-                    <h3 className="flex-end"><span className="spectral big-letter">6</span><span>The Will to Power</span></h3>
-                    <p className="small-grey-font">Is there a unified system that underpins Nietzsche’s thought? Well, the ‘will to power’ is a good candidate. Day six will discuss how the will to power plays a crucial role throughout Nietzsche’s writings, and summarize where scholars stand on it today. </p>
+            <div className="small-mar">
+                <div className="two-col big-2-col page-center row-reverse">
+                    <div>
+                        <h3 className="flex-end"><span className="spectral big-letter">2</span><span>The Apollonian & Dionysian</span></h3>
+                        <p className="spectral">On day two we’ll start digging deeper into Nietzsche’s core ideas. We’ll begin with his early distinction between the Apollonian and Dionysian, which acts not only as a supremely illuminating way to consider life and art, but also as a precursor to his later ideas.</p>
+                    </div>
+                    <div>
+                    <GatsbyImage
+                        image={data.chapter2.childImageSharp.gatsbyImageData}
+                        alt="The Apollonian & Dionysian" />
+                    </div>
                 </div>
             </div>
+            <div className="small-mar">
+                <div className="two-col big-2-col page-center row-reverse">
+                    <div>
+                        <h3 className="flex-end"><span className="spectral big-letter">3</span><span>Perspectivism</span></h3>
+                        <p className="spectral">Do human beings have access to objective truth? Day three will consider Nietzsche’s fascinating theory of perspectivism, a highly sophisticated account of what truth is and how we can access it — an account that continues to excite scholars today.</p>
+                    </div>
+                    <div>
+                    <GatsbyImage
+                        image={data.chapter3.childImageSharp.gatsbyImageData}
+                        alt="Nietzsche’s Perspectivism" />
+                    </div>
+                </div>
+            </div>
+            <div className="small-mar">
+                <div className="two-col big-2-col page-center row-reverse">
+                    <div>
+                        <h3 className="flex-end"><span className="spectral big-letter">4</span><span>Master & Slave Morality</span></h3>
+                        <p className="spectral">Where do morals come from? Nietzsche’s profound contribution to answering this question will be discussed on day four, where we’ll assess his takedown of traditional values like altruism, as well as his concepts of the ascetic ideal, master morality, and slave morality.</p>
+                    </div>
+                    <div>
+                    <GatsbyImage
+                        image={data.chapter4.childImageSharp.gatsbyImageData}
+                        alt="Master & Slave Morality" />
+                    </div>
+                </div>
+            </div>
+            <div className="small-mar">
+                <div className="two-col big-2-col page-center row-reverse">
+                    <div>
+                        <h3 className="flex-end"><span className="spectral big-letter">5</span><span>God is Dead & the Übermensch</span></h3>
+                        <p className="spectral">What should we value in a Godless world — and why? Nietzsche’s attempt to provide a ‘revaluation of values’ will be discussed on day five, where we’ll consider his famous ideas that God is Dead, the eternal recurrence, and the Übermensch (superman).</p>
+                    </div>
+                    <div>
+                    <GatsbyImage
+                        image={data.chapter5.childImageSharp.gatsbyImageData}
+                        alt="God is Dead & the Übermensch" />
+                    </div>
+                </div>
+            </div>
+            <div className="small-mar">
+                <div className="two-col big-2-col page-center row-reverse">
+                    <div>
+                        <h3 className="flex-end"><span className="spectral big-letter">6</span><span>The Will to Power</span></h3>
+                        <p className="spectral">Is there a unified system that underpins Nietzsche’s thought? Well, the ‘will to power’ is a good candidate. Day six will discuss how the will to power plays a crucial role throughout Nietzsche’s writings, and summarize where scholars stand on it today.</p>
+                    </div>
+                    <div>
+                    <GatsbyImage
+                        image={data.chapter6.childImageSharp.gatsbyImageData}
+                        alt="The Will to Power" />
+                    </div>
+                </div>
+            </div>
+            
             <div className="page-center small-pad mid-width text-center">
             <h2 className="small-mar">Register Interest Now to Secure Your Launch Discount</h2>
                     <div className="separator"></div>
@@ -121,6 +170,36 @@ export const query = graphql`{
   about: file(relativePath: {eq: "intro-to-nietzsche-chap1-full.png"}) {
     childImageSharp {
       gatsbyImageData(width: 600, quality: 90, layout: CONSTRAINED, placeholder: BLURRED)
+    }
+  }
+  chapter1: file(relativePath: {eq: "Nietzsche-Chapter-1.png"}) {
+    childImageSharp {
+      gatsbyImageData(width: 600, quality: 90, layout: CONSTRAINED)
+    }
+  }
+  chapter2: file(relativePath: {eq: "Nietzsche-Chapter-2.png"}) {
+    childImageSharp {
+      gatsbyImageData(width: 600, quality: 90, layout: CONSTRAINED)
+    }
+  }
+  chapter3: file(relativePath: {eq: "Nietzsche-Chapter-3.png"}) {
+    childImageSharp {
+      gatsbyImageData(width: 600, quality: 90, layout: CONSTRAINED)
+    }
+  }
+  chapter4: file(relativePath: {eq: "Nietzsche-Chapter-4.png"}) {
+    childImageSharp {
+      gatsbyImageData(width: 600, quality: 90, layout: CONSTRAINED)
+    }
+  }
+  chapter5: file(relativePath: {eq: "Nietzsche-Chapter-5.png"}) {
+    childImageSharp {
+      gatsbyImageData(width: 600, quality: 90, layout: CONSTRAINED)
+    }
+  }
+  chapter6: file(relativePath: {eq: "Nietzsche-Chapter-6.png"}) {
+    childImageSharp {
+      gatsbyImageData(width: 600, quality: 90, layout: CONSTRAINED)
     }
   }
 }
