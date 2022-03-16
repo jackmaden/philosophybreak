42a2ba0b1c9a63534ece5ede8e0472cf3899c9cf
Jack Maden <jamaden92@gmail.com>
2022-03-16 18:07:47 +0000

    ty-update

--- src/pages/thank-you/index.js
+++ src/pages/thank-you/index.js
@@ -18,7 +18,7 @@ const Page = () => {
                     <div className="page-center">
                         <h1 className="mid-width">Thank You</h1>
                         <div className="separator"></div>
-                        <p className="large-grey-font mid-width">Thank you very much for enrolling in our 3-part email course, philosopher! Our welcome email will be landing in your inbox shortly. In the meantime, be sure to follow Philosophy Break on <a href="https://www.instagram.com/philosophybreak/" target="_blank" rel="noopener noreferrer">Instagram</a> and <a target="_blank" rel="noopener noreferrer" href="https://twitter.com/philosophybreak">Twitter</a> to learn about philosophy directly in your newsfeed.</p>
+                        <p className="large-grey-font mid-width">Thank you very much for enrolling in our 3-part email course, philosopher! Our welcome email will be landing in your inbox shortly. Please allow up to 30 minutes for delivery, and be advised that some email filters can send initial emails from unknown senders to your junk folder. If you encounter any issues, just drop us a line at hello@philosophybreak.com, and we’ll be on hand to help.</p><p className="large-grey-font mid-width"> In the meantime, be sure to follow Philosophy Break on <a href="https://www.instagram.com/philosophybreak/" target="_blank" rel="noopener noreferrer">Instagram</a> and <a target="_blank" rel="noopener noreferrer" href="https://twitter.com/philosophybreak">Twitter</a> to learn about philosophy directly in your newsfeed.</p>
                     </div>
                 </div>
             </div>
