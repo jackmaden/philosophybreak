b047fd7e3ebf0e204f5e0e94d26f1d74820929b5
Jack Maden <jamaden92@gmail.com>
2022-10-03 13:38:09 +0100

    home-copy

--- src/pages/index.js
+++ src/pages/index.js
@@ -51,7 +51,7 @@ const Page = ( {data} ) => {
               <div className="text-center desktop-no-display">
                 <div className="separator"></div>
               </div>
-              <p className="large-grey-font">At last. You made it. The perfect place to begin your philosophical journey. Start by discovering exactly what philosophy is and how it can improve your life with our free, 3-lesson email course:</p>
+              <p className="large-grey-font">Philosophy can be transformative. Are you up for expanding your mind? Discover exactly what philosophy is and how it can improve your life with our free, 3-lesson email course:</p>
               <HomeSubscriptionForm />
             </div>
             <div>
