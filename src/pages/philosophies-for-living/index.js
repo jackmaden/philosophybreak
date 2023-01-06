7cc8370ab923c26b1b03fa86f557e95f5191480f
Jack Maden <jamaden92@gmail.com>
2023-01-06 11:00:44 +0000

    p4l

--- src/pages/philosophies-for-living/index.js
+++ src/pages/philosophies-for-living/index.js
@@ -10,16 +10,16 @@ const Page = ({ location }) => {
     
     return (
     <>
-    <Seo title="Philosophies for Living, from Aristotle to Confucius" description="Discover practical wisdom from 7 of the world’s most influential philosophies — including Stoicism, Buddhism, and Existentialism — to help you find meaning, purpose, and live a good life." pathname={location.pathname} />
+    <Seo title="Philosophies for Living: the Ultimate Guide to Choosing Your Personal Philosophy" description="Discover practical wisdom from 7 of the world’s most influential philosophies — including Stoicism, Buddhism, and Existentialism — to help you find meaning, purpose, and live a good life." pathname={location.pathname} />
     <Layout>
     <NavigationSimple/>
         {/*page title*/}
         <div className="top-title darkradial-background course" id="top">
             <div className="page-center text-center">
                 <div className="mid-width small-pad-bottom">
-                    <p className="verified no-mar-top">COMING SOON</p><br/><span className="tag time"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512"><path fill="#fff" d="M624 416H381.54c-.74 19.81-14.71 32-32.74 32H288c-18.69 0-33.02-17.47-32.77-32H16c-8.8 0-16 7.2-16 16v16c0 35.2 28.8 64 64 64h512c35.2 0 64-28.8 64-64v-16c0-8.8-7.2-16-16-16zM576 48c0-26.4-21.6-48-48-48H112C85.6 0 64 21.6 64 48v336h512V48zm-64 272H128V64h384v256z"/></svg>7-CHAPTER COURSE</span><div className="separator course chapter"></div>
-                    <h1>Philosophies for Living, from Aristotle to Confucius</h1>
-                    <p className="spectral">Discover practical wisdom from 7 of the world’s most influential philosophies — including Stoicism, Buddhism, and Existentialism — to help you find meaning, purpose, and live a good life.</p> 
+                    <p className="verified no-mar-top">COMING SOON</p><br/><span className="tag time"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512"><path fill="#fff" d="M624 416H381.54c-.74 19.81-14.71 32-32.74 32H288c-18.69 0-33.02-17.47-32.77-32H16c-8.8 0-16 7.2-16 16v16c0 35.2 28.8 64 64 64h512c35.2 0 64-28.8 64-64v-16c0-8.8-7.2-16-16-16zM576 48c0-26.4-21.6-48-48-48H112C85.6 0 64 21.6 64 48v336h512V48zm-64 272H128V64h384v256z"/></svg>COURSE & COMMUNITY</span><div className="separator course chapter"></div>
+                    <h1>Philosophies for Living: the Ultimate Guide to Choosing Your Personal Philosophy</h1>
+                    <p className="spectral">Find meaning, purpose, and learn how to live a good life by exploring 7 of the world’s most influential philosophies for living — including Stoicism, Buddhism, and Existentialism. Register your interest now:</p> 
                     
                     <CoursePromoForm />
                 </div>
@@ -59,7 +59,7 @@ const Page = ({ location }) => {
                     <div className="mid-width text-center large-pad">
                         <h2>Register Now to Stay Notified and Secure a Launch Discount</h2>
                         <div className="separator"></div>
-                        <p className="spectral">Coming soon: find meaning, purpose, and learn how to live a good life with practical wisdom from Aristotle, Epicurus, Stoicism, Confucius, Buddhism, Existentialism, and Effective Altruism.</p> 
+                        <p className="spectral">Explore and compare practical wisdom from Aristotelianism, Epicureanism, Stoicism, Confucianism, Buddhism, Existentialism, and Effective Altruism. Register your interest now:</p> 
                         
                         <CoursePromoForm />
                     </div>
