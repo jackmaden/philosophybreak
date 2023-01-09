63038b1a30a28d47c045a7244100aa6d1372a0aa
Jack Maden <jamaden92@gmail.com>
2023-01-09 12:01:38 +0000

    pfl-update

--- src/components/panelNietzsche.js
+++ src/components/panelNietzsche.js
@@ -26,7 +26,7 @@ const PanelNietzsche = props => (
                         <div className="mobile-no-display">
                             <p className="verified no-mar-top">NEW!</p><span className="tag time new" style={{"color": "#001d30"}}><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512"><path fill="#001d30" d="M624 416H381.54c-.74 19.81-14.71 32-32.74 32H288c-18.69 0-33.02-17.47-32.77-32H16c-8.8 0-16 7.2-16 16v16c0 35.2 28.8 64 64 64h512c35.2 0 64-28.8 64-64v-16c0-8.8-7.2-16-16-16zM576 48c0-26.4-21.6-48-48-48H112C85.6 0 64 21.6 64 48v336h512V48zm-64 272H128V64h384v256z"/></svg>6-DAY COURSE</span><div className="separator course new mobile-center-margin"></div>
                         </div>
-                        <Link className="heading-link" to="/introduction-to-nietzsche/"><h2>Your Myth-Busting Guide to Nietzsche & His 5 Greatest Ideas</h2></Link>
+                        <Link className="heading-link" to="/introduction-to-nietzsche/"><h3>Your Myth-Busting Guide to Nietzsche & His 5 Greatest Ideas</h3></Link>
                         
                         
                         <GatsbyImage
