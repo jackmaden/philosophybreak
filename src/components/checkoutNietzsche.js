2136e44f4105cfb2090d94344f9a038a87271a54
Jack Maden <jamaden92@gmail.com>
2023-02-01 16:29:48 +0000

    course-update

--- src/components/checkoutNietzsche.js
+++ src/components/checkoutNietzsche.js
@@ -2,7 +2,7 @@ import React from "react"
 
 const CheckoutNietzsche = () => (
   <>
-    <a className="button primary full-width top-icon-align" href="https://buy.stripe.com/bIY6p7dNC0eJbvOfZ2">
+    <a className="button primary full-width top-icon-align" href="https://buy.stripe.com/8wMaFn8tibXr0Ra8wD">
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M400 224h-24v-72C376 68.2 307.8 0 224 0S72 68.2 72 152v72H48c-26.5 0-48 21.5-48 48v192c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48V272c0-26.5-21.5-48-48-48zm-104 0H152v-72c0-39.7 32.3-72 72-72s72 32.3 72 72v72z"/></svg>Get Instant Access for $29
     </a>
     <p className="tiny-mar-top" style={{marginBottom: 0}}><span style={{color: "#ffc536"}}>&#9733;&#9733;&#9733;&#9733;&#9733;</span> <span style={{fontSize: "16px"}}>(12 reviews)</span></p>
