1c1dadbb5920abf34fbe08c75b7e6ae818b3faef
Jack Maden <jamaden92@gmail.com>
2023-07-11 20:42:29 +0100

    pfl-copy

--- src/components/panelPFL.js
+++ src/components/panelPFL.js
@@ -33,7 +33,7 @@ export default function PanelPFL(props) {
                             fadeIn={false}
                             alt="How to Live a Good Life" />
 
-                        <p className="large-grey-font no-mar-top-mob">Quickly compare the profound wisdom of Stoicism, Existentialism, Buddhism and beyond to see if they might enrich your own way of life. Access our popular new guide:</p>
+                        <p className="large-grey-font no-mar-top-mob">Quickly enrich your personal philosophy by comparing the wisdom of Stoicism, Existentialism, Buddhism and beyond. Explore our popular new guide now:</p>
 
 
                         <Link className="button primary full-width" to="/how-to-live-a-good-life/"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M504 256C504 119 393 8 256 8S8 119 8 256s111 248 248 248 248-111 248-248zm-448 0c0-110.5 89.5-200 200-200s200 89.5 200 200-89.5 200-200 200S56 366.5 56 256zm72 20v-40c0-6.6 5.4-12 12-12h116v-67c0-10.7 12.9-16 20.5-8.5l99 99c4.7 4.7 4.7 12.3 0 17l-99 99c-7.6 7.6-20.5 2.2-20.5-8.5v-67H140c-6.6 0-12-5.4-12-12z"/></svg>Get Instant Access</Link>
