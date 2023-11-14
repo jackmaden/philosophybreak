06cd6ca3abfa99a1bdca8d73707af72dada83edd
Jack Maden <jamaden92@gmail.com>
2023-11-14 17:02:38 +0000

    sparkloop

--- src/components/layout.js
+++ src/components/layout.js
@@ -15,7 +15,10 @@ const Layout = ({ children }) => (
             <script async type="text/javascript" src="https://app.termly.io/embed.min.js" data-auto-block="off" data-website-uuid="9e6e8835-b3fb-4631-b74a-0755bf89b753"></script>*/}
 
             {/*MEDIAVINE*/}
-            <script type="text/javascript" async="async" data-noptimize="1" data-cfasync="false" src="//scripts.mediavine.com/tags/philosophy-break.js"></script>
+            <script type="text/javascript" async="async" data-noptimize="1" data-cfasync="false" src="//scripts.mediavine.com/tags/philosophy-break.js" />
+
+            {/*SPARKLOOP*/}
+            <script async src="https://js.sparkloop.app/team_6e16a193d55e.js" data-sparkloop />
         </Helmet>
         {children}
         <Footer />
